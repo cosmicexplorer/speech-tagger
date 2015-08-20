39f535c2fa2168f9163614b140a777ec9c4ffccc
Danny McClanahan <danieldmcclanahan@gmail.com>
2015-08-20 06:43:12 -0400

    fix install.sh

--- install.sh
+++ install.sh
@@ -5,16 +5,18 @@ version="0.0.0"
 jar_url="$gh_pfx/$version/speech-tagger-0.0.0-SNAPSHOT-standalone.jar"
 model_url="$gh_pfx/$version/english-left3words-distsim.tagger"
 tags_url="$gh_pfx/$version/penn_treebank_tags.json"
-bindir="/usr/bin/speech-tagger"
+bindir="/usr/bin/speech-tagger-lib"
 
-mkdir "$bindir"
+mkdir -p "$bindir"
 for url in "$jar_url" "$model_url" "$tags_url"; do
-  wget "$url" -o "$bindir"
+  wget "$url" -P "$bindir"
 done
 
 cat >/usr/bin/speech-tagger <<EOF
 #!/bin/sh
-prefix="/usr/bin/speech-tagger"
-java -jar "$prefix/speech-tagger-0.0.0-SNAPSHOT-standalone.jar" \
-  "$prefix/english-left3words-distsim.tagger" "$prefix/penn_treebank_tags.json"
+prefix="/usr/bin/speech-tagger-lib"
+java -jar "\$prefix/speech-tagger-0.0.0-SNAPSHOT-standalone.jar" \
+  "\$prefix/english-left3words-distsim.tagger" \
+  "\$prefix/penn_treebank_tags.json"
 EOF
+chmod +x /usr/bin/speech-tagger
